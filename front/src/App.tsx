4761b33298b70075fde606c9fa5ff5c385ad77c8
aoi-netai <aoi256jp@Gmail.com>
2026-06-11 11:58:57 +0000

    リアルタイムでページが自動更新されるように修正

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>okita</h1>
+          <h1>test1</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+      protocol: 'ws',
+    },
+    watch: {
+      usePolling: true,
+    },
   },
 })
